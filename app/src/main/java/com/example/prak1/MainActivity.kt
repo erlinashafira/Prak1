7ce92907e8fa0df1f9bd74bad06b39a0259864b5
errerew <erlina31acc@gmail.com>
2026-09-24 15:16:50 +0700

    mengubah nama variabe; dari innerpadding jadi bodyPadding

--- app/src/main/java/com/example/prak1/MainActivity.kt
+++ app/src/main/java/com/example/prak1/MainActivity.kt
@@ -19,10 +19,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Prak1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) { bodyPadding ->
                     Greeting(
                         name = "Erlina",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(bodyPadding)
                     )
                 }
             }
